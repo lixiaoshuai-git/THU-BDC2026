3e953d1a31db3b4717d4629d5f0ed56062dc6659
lixiaoshuai <2499315638@qq.com>
2026-06-03 13:40:57 +0800

    团队新牛逼模型

--- data/split_train_test.py
+++ data/split_train_test.py
@@ -29,20 +29,20 @@ def parse_args() -> argparse.Namespace:
 	parser.add_argument(
 		"--train-end",
 		type=str,
-		default="2026-03-06",
-		help="训练集结束日期，默认 2026-03-06",
+		default="2026-05-22",
+		help="训练集结束日期，默认 2026-05-22",
 	)
 	parser.add_argument(
 		"--test-start",
 		type=str,
-		default="2026-03-09",
-		help="测试集开始日期，默认 2026-03-09",
+		default="2026-05-25",
+		help="测试集开始日期，默认 2026-05-25",
 	)
 	parser.add_argument(
 		"--test-end",
 		type=str,
-		default="2026-03-13",
-		help="测试集结束日期，默认 2026-03-13",
+		default="2026-05-29",
+		help="测试集结束日期，默认 2026-05-29",
 	)
 	return parser.parse_args()
 
